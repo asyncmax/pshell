2dd3bc78f935d5630d2948b36820ff483fe469f2
asyncmax <kjnk@Naekwons-Mac-mini.local>
2016-09-15 15:46:35 -0700

    Update examples

--- examples/7.js
+++ examples/7.js
@@ -3,5 +3,5 @@ var shell = require("pshell").context({echoCommand: false, ignoreError: true});
 shell('node -e "process.exit(1)"').then(res => {
   console.log("exit code:", res.code);
 }).catch(err => {
-  console.error("error occurred:", err);
+  console.error("error occurred:", err.message);
 });
